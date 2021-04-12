acb8953cef4378d35da3a99a11d0a94d6f8e5961
Renzo Fajardo <renzofajardo613@gmail.com>
2021-04-12 20:31:49 -0300

    .

--- Open-ClosedPrinciple/dominio/dominio/GestorOrdenar.java
+++ Open-ClosedPrinciple/dominio/dominio/GestorOrdenar.java
@@ -3,3 +3,7 @@ package dominio;
 public interface GestorOrdenar {
 	public void ordenar();
 }
+//Este interface es la encargada de la aplicacion del principio open-closed
+//Lo que permite es que cuando soliciten una nueva forma de ordenar
+//No tengamos que modificar ningun metodo, solamente basta con crear una clase y aplicar
+//el metodo ordenar
